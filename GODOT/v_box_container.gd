ccef483bf5a79856afd65d205404419b4f60b7a6
helppls551 <gideon.gideonovich@mail.ru>
2026-03-05 18:42:53 +0300

    Update v_box_container.gd

--- GODOT/v_box_container.gd
+++ GODOT/v_box_container.gd
@@ -4,11 +4,10 @@ var dragging = false
 var drag_offset = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func _process(delta: float) -> void:
 	pass
-
 func _on_closebut_pressed() -> void:	
 	$".".hide()
 func _on_hidebut_pressed() -> void:
